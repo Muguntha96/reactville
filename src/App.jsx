9c3a359f2cffdec3a97b67dc94d8b95b5221d8bd
Muguntha96 <muguntharamkumar1996@gmail.com>
2023-10-04 21:40:25 -0300

    complete routes and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import './styles/App.css'
-
+import { Routes,Route } from 'react-router-dom'
+import BurgerShop from './Components/BurgerShop/BurgerShop'
+import Nav from './Components/Nav/Nav'
 // Components
 import Landing from './Components/Landing/Landing'
-
 const App = () => {
   return (
+    <>
+    <Nav />
     <main>
-      <Landing />
-    </main>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/burgers' element={<BurgerShop />} />
+      </Routes>
+        </main>
+        </>
   )
 }
-
 export default App
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,9 +1,13 @@
 // Components & Assets
 import Logo from '../../assets/react-logo.png'
+import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <nav className="navigation-bar">
+     <NavLink to='/'> <img src={Logo} alt='logo-react'/></NavLink>
+     <NavLink to='/burgers'>	
+BURGER SHOP</NavLink>
 
     </nav>
   )
